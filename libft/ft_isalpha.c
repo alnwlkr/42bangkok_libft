6ec15cb02b3bf459c6ac7882877368a071181cfa
alnwlkr <fostgeo5@gmail.com>
2022-09-28 06:18:41 +0700

    is_alpha

---
+++ libft/ft_isalpha.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: panoma <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 06:16:01 by panoma            #+#    #+#             */
+/*   Updated: 2022/09/28 06:18:11 by panoma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
+
+static int	ft_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+int	ft_isalpha(int c)
+{
+	if (ft_isupper(c) || ft_islower(c))
+		return (1);
+	return (0);
+}
